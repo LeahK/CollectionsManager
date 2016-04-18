18cf9a0fb2c504bca0a9c35de9618c43db0d23e2
justin@webshopapps.com <justin@webshohpapps.com>
2016-04-18 12:13:04 -0400

    Finished testing items and fixing them, have a few strange errors that I cant fix happening intermitently. My plan is to add refresh buttons to all views so that way if there is a problem with the db they can sync to fix their data.

--- main/src/main/java/com/example/wduello/collectionmanager/ItemListActivity.java
+++ main/src/main/java/com/example/wduello/collectionmanager/ItemListActivity.java
@@ -3,6 +3,8 @@ package com.example.wduello.collectionmanager;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
 
@@ -20,6 +22,14 @@ public class ItemListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new ItemListFragment();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // if back button was pressed restart the collections activity
+        Intent i = new Intent(this, ActivityCollections.class);
+        startActivity(i);
     }
 
 
